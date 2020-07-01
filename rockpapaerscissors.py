984da4924ffeaeccdb83ea4788a1c31e93cd4c4f
2016225071_김종한 <jong7721@naver.com>
2020-07-01 13:41:59 +0900

    [random-play-01] Set a random play to the computer

--- rockpapaerscissors.py
+++ rockpapaerscissors.py
@@ -1,7 +1,10 @@
+from random import randint
+
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
 # assign a random play to the computer
+computer = play[randint(0, 2)]
 computer = "Rock"
 print('Computer: {}'.format(computer))
 
