5a11a03e2b55cf51c2bde56f8a0354047a6fec36
2016225071_김종한 <jong7721@naver.com>
2020-07-01 13:58:13 +0900

    [compare-02] Add the rock case

--- rockpapaerscissors.py
+++ rockpapaerscissors.py
@@ -1,16 +1,13 @@
-from random import randint
-
 # list of play options
 play = ["Rock", "Paper", "Scissors"]
 
 # assign a random play to the computer
-computer = play[randint(0, 2)]
 computer = "Rock"
 print('Computer: {}'.format(computer))
 
 # get the user input
-player = input("Rock, Paper, Scissors? ")
-print('player: {}'.format(player))
+player = "Paper"
+print('Player: {}'.format(player))
 
 # tie
 if player == computer:
